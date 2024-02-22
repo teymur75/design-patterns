a64f5190685ff89b88806582e79093637c6b9721
teymur75 <teymurvr@code.edu.az>
2024-02-22 17:07:29 +0400

    DecoratorPattern

---
+++ DecoratorPattern/Computers/Computer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Computers
+{
+    public class Computer
+    {
+        public void Start()
+        {
+            Console.WriteLine($"{GetType().Name} is starting");
+        }
+
+        public void ShutDown()
+        {
+            Console.WriteLine($"{GetType().Name} is shuttingDown");
+        }
+    }
+
+    public class Laptop:Computer
+    {
+        public void OpenLid()
+        {
+            Console.WriteLine($"{GetType().Name}'s lid is opening");
+        }
+
+        public void CloseLid()
+        {
+            Console.WriteLine($"{GetType().Name} 's lid is closing");
+        }
+    }
+
+    public class LaptopDecorator:Laptop
+    {
+        public virtual void OpenLid()
+        {
+            //do something
+            base.OpenLid();
+        }
+
+        public virtual void CloseLid()
+        {
+            //do something
+            base.CloseLid();
+        }
+    }
+
+    public class DellLaptop:LaptopDecorator
+    {
+        public override void CloseLid()
+        {
+            base.CloseLid();
+            Console.WriteLine("Dell Laptop is sleeping");
+        }
+
+        public override void OpenLid()
+        {
+            base.OpenLid();
+            Console.WriteLine("DEll laptop is waking");
+        }
+    }
+
+    public class AppleLaptop : LaptopDecorator
+    {
+        public void OpenLid()
+        {
+            //do something
+            base.OpenLid();
+        }
+
+        public void CloseLid()
+        {
+            //do something
+            base.CloseLid();
+        }
+    }
+}
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,26 +1,37 @@
 ﻿#region BuilderPattern
 
 
-using BuilderPattern.Method1;
+//using BuilderPattern.Method1;
 
-var eb = new EndPointBuilder("https://localhost");
-eb.Append("api")
-    .Append("v1")
-    .Append("user")
-    .AppendParams("id", "5")
-    .AppendParams("username", "Teymur");
+//var eb = new EndPointBuilder("https://localhost");
+//eb.Append("api")
+//    .Append("v1")
+//    .Append("user")
+//    .AppendParams("id", "5")
+//    .AppendParams("username", "Teymur");
 
-var url = eb.Build();
+//var url = eb.Build();
 
-//Console.WriteLine(url);
+////Console.WriteLine(url);
 
-EmployeeBuilderM1 empBuilder = new();
-var employee= empBuilder
-    .SetEmailAdress("tt@mail.com")
-    .SetUsername("Tete")
-    .BuildEmployee();
+//EmployeeBuilderM1 empBuilder = new();
+//var employee= empBuilder
+//    .SetEmailAdress("tt@mail.com")
+//    .SetUsername("Tete")
+//    .BuildEmployee();
 
-Console.WriteLine(employee);
+//Console.WriteLine(employee);
+
+
+#endregion
+
+#region DecoratorPattern
+
+//BIr metodun isleme cercivesini boyutmek 
+// Decorator Pattern istifade ederek bir base calsin metodunun ondan toreyen herbir 
+//classlarda override olunaraq ferqli meqsedlerde istifade olunmasidir,Eslinde cox istifade etdiyimiz ama az bildiyimiz
+//bir design patterndir
+
 
 
 #endregion
